e54de7f459f86643efadf3cc97b29744e0412032
Lefcott <leandro.cotti@widergy.com>
2020-09-23 03:39:45 -0300

    renames

--- src/sectionSwitcher.js
+++ src/sectionSwitcher.js
@@ -3,8 +3,8 @@ import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { SECTIONS } from './store/constants';
-import Menu from './components/Menu';
-import CreateMap from './components/CreateMap';
+import Menu from './screens/Menu';
+import CreateMap from './screens/CreateMap';
 
 export default function SectionSwitcher() {
   const section = useSelector(({ section }) => section);
